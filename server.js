6871ed45e654425babeea8d5b1a9e0fd496a1c7d
adepurana <adepurana@gmail.com>
2017-03-13 16:36:54 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 // set up ======================================================================
 // get all the tools we need
+process.env.TZ = 'Asia/Jakarta' 
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
